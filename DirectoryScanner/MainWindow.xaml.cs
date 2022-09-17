dc4a292f2c26fd892e2eff16dae1c4a245c3cf5c
Вероника Богданович <vera022003@gmail.com>
2022-09-17 20:47:03 +0300

    Добавьте файлы проекта.

---
+++ DirectoryScanner/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirectoryScanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel = new ViewModel();
+        }
+
+        private void startTracing(object sender, RoutedEventArgs e)
+        {
+            viewModel.startTracing("C:\\Users\\Veronika\\Downloads");
+
+        }
+    }
+
+    public class Article
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ DirectoryScanner/ViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Xml.Linq;
+
+namespace DirectoryScanner
+{
+    public class ViewModel: INotifyPropertyChanged
+    {
+        
+
+        private file Dir;
+        public file dir { get { return Dir; }  set { dir = Dir; OnPropertyChanged(); } }
+  
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ViewModel()
+        {
+
+        }
+        public void startTracing(string path)
+        {
+            Dir = searchDirectory(path);
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private file searchDirectory(string path)
+        {
+            var searchedDirectory = new file(path);
+
+            string[] directoryList = Directory.GetDirectories(searchedDirectory.Name);
+            foreach (var directory in directoryList)
+            {
+                searchedDirectory.files.Add(searchDirectory(directory));
+
+            }
+            string[] filelist = Directory.GetFiles(searchedDirectory.Name);
+            foreach (var _path in filelist)
+            {
+                searchedDirectory.files.Add(new ViewModel.file(System.IO.Path.GetFileName(_path)));
+            }
+            return searchedDirectory;
+        }
+
+
+        public class file
+        {
+            public file(string name)
+            {
+                Name = name;
+                files = new ObservableCollection<file>();
+            }
+            public string Name { get; set; }
+            public ObservableCollection<file> files { get; set; }
+        }
+
+      
+
+    }
+}
